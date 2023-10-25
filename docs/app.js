344edd7950590f2cbeb926361e73289750e2b58e
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-10-25 16:00:17 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,23 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB_YIlap7tI3WNckTq1A9XEBjQeaIK7g7w",
+    authDomain: "parking360-bc4c6.firebaseapp.com",
+    projectId: "parking360-bc4c6",
+    storageBucket: "parking360-bc4c6.appspot.com",
+    messagingSenderId: "1003326374942",
+    appId: "1:1003326374942:web:5cb193cdcfc43892bbda0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 baseDatos = JSON.parse( window.localStorage.getItem("DB_users"));
 
 
@@ -35,6 +55,8 @@ function cargarDatosDB() {
         }
     }
 }
+
+
 
 function login() {  
         let usuario = document.getElementById("email-login").value;
