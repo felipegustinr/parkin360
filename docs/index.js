0a2c5a25ce7c39846692a8e0c4bd2354f469ad0d
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-10-25 15:13:08 -0500

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB_YIlap7tI3WNckTq1A9XEBjQeaIK7g7w",
+    authDomain: "parking360-bc4c6.firebaseapp.com",
+    projectId: "parking360-bc4c6",
+    storageBucket: "parking360-bc4c6.appspot.com",
+    messagingSenderId: "1003326374942",
+    appId: "1:1003326374942:web:5cb193cdcfc43892bbda0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
