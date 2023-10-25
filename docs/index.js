230b93de99db5755893f5917b3f42f3d730388f4
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-10-25 17:45:38 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,21 @@
-window.addEventListener('DOMContentLoaded', ()=> {
-    
+import { saveUser } from "./app.js";
+window.addEventListener('DOMContentLoaded', () => {
+
 })
 
 const registerForm = document.getElementById('registerUser');
 
 registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    console.log('Enviando datos')
-})
+    
+    const email = registerForm['email-reg'];
+    const password = registerForm['password-reg'];
+    const confirmPass = registerForm['confirmPass-reg'];
+    const direccion = registerForm['Direccion'];
+    const ciudad = registerForm['Ciudad'];
+    const pais = registerForm['Pais'];
+    const zipcode = registerForm['ZipCode'];
+    
+    saveUser(email.value,password.value,confirmPass.value,direccion.value,ciudad.value,pais.value,zipcode.value);
+    
+});
