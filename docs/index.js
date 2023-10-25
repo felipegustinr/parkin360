284986fbb4e185c11783cfeec82320b4db3932f4
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-10-25 16:00:19 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,18 +1,11 @@
+window.addEventListener('DOMContentLoaded', ()=> {
+    
+})
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const registerForm = document.getElementById('registerUser');
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyB_YIlap7tI3WNckTq1A9XEBjQeaIK7g7w",
-    authDomain: "parking360-bc4c6.firebaseapp.com",
-    projectId: "parking360-bc4c6",
-    storageBucket: "parking360-bc4c6.appspot.com",
-    messagingSenderId: "1003326374942",
-    appId: "1:1003326374942:web:5cb193cdcfc43892bbda0f"
-};
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+    console.log('Enviando datos')
+})
